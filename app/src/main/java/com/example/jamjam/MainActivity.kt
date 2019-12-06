affe4c04e0120e57d22548629a5e26a8966b95c5
Rick <rgreaves@google.com>
2019-12-05 16:09:04 -0800

    I dont know what i did, but it stopped yelling at me.

--- app/src/main/java/com/example/jamjam/MainActivity.kt
+++ app/src/main/java/com/example/jamjam/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.jamjam
 
-import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.widget.Toolbar
-import com.google.cloud.storage.StorageOptions
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.util.Size
+import android.graphics.Matrix
+import android.util.Log
+import android.view.*
+import androidx.camera.core.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.io.File
+import java.util.concurrent.Executors
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// This is an array of all the permission specified in the manifest.
+private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.INTERNET )
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,19 +42,146 @@ class MainActivity : AppCompatActivity() {
         //Generate Random ID on load
         reset_id()
 
+        //Start the camera
+        viewFinder = findViewById(R.id.view_finder)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Every time the provided texture view changes, recompute layout
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+
     }
 
-    fun quickstart(bucketName: String) {
-        // [START storage_quickstart]
-        // import com.google.cloud.storage.StorageOptions
-        val storage = StorageOptions.getDefaultInstance().service
-        val bucket = storage.get(bucketName) ?: error("Bucket $bucketName does not exist.")
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var viewFinder: TextureView
 
-        println("Listing all blobs in bucket $bucketName:")
-        bucket.list().iterateAll().forEach { blob ->
-            println("${blob.name} (content-type: ${blob.contentType}, size: ${blob.size})")
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
         }
-        // [END storage_quickstart]
+    }
+
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(640, 480))
+        }.build()
+
+
+        // Build the viewfinder use case
+        val preview = Preview(previewConfig)
+
+        // Every time the viewfinder is updated, recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                // We don't set a resolution for image capture; instead, we
+                // select a capture mode which will infer the appropriate
+                // resolution based on aspect ration and requested mode
+                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+            }.build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+
+
+        //Image Take Handler
+        btnTakePic.setOnClickListener {
+            //Setup the event
+            Toast.makeText(this,"Starting Process",Toast.LENGTH_SHORT).show()
+            btnTakePic.isEnabled = false
+
+            //Take the picture
+            val file = File(externalMediaDirs.first(),
+                "$mapid.jpg")
+
+            imageCapture.takePicture(file, executor,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(
+                        imageCaptureError: ImageCapture.ImageCaptureError,
+                        message: String,
+                        exc: Throwable?
+                    ) {
+                        val msg = "Photo capture failed: $message"
+                        Log.e("CameraXApp", msg, exc)
+                        viewFinder.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        val msg = "Photo capture succeeded: ${file.absolutePath}"
+                        Log.d("CameraXApp", msg)
+                        viewFinder.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
+                })
+
+            //Now pass the image to upload
+
+
+            //Re-enable upload
+            btnTakePic.isEnabled = true
+        }
+
+
+        // Bind use cases to lifecycle
+        CameraX.bindToLifecycle(this, preview, imageCapture)
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when(viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -81,4 +218,5 @@ class MainActivity : AppCompatActivity() {
         //Set global value
         mapid = newID
     }
+
 }
